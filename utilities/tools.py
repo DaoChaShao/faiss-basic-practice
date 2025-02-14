ad23b2395aa8264cab6ab25c08ac5e361d1876fb
wli916 <change31@gmail.com>
2025-02-14 17:48:49 +1300

    feat: add the advanced functions such as: Timer and SeedNumpy

--- utilities/tools.py
+++ utilities/tools.py
@@ -1,41 +1,80 @@
+from faiss import Index, index_factory, METRIC_L2, write_index, read_index
 from numpy import random, ndarray, array
-import faiss
+from time import perf_counter
 
 
-def faiss_index_creator(dimension: int, method: str = "Flat") -> faiss.Index:
+def faiss_index_creator(dimension: int, method: str = "Flat") -> Index:
     """ Creates a Faiss index based on the given dimension and method. """
-    index = faiss.index_factory(dimension, method, faiss.METRIC_L2)
+    index = index_factory(dimension, method, METRIC_L2)
     # index = IndexFlatL2(dimension)
     return index
 
 
-def faiss_index_adder(index: faiss, amount: int, dimension: int, vectors_type: str = "float32"):
+def faiss_index_adder(index, amount: int, dimension: int, vectors_type: str = "float32"):
     """ Adds vectors to a Faiss index. """
     vectors = random.rand(amount, dimension).astype(vectors_type)
     index.add(vectors)
 
 
-def faiss_index_search(index: faiss, query: ndarray, top_n: int) -> tuple[ndarray, ndarray]:
+def faiss_index_search(index, query: ndarray, top_n: int) -> tuple[ndarray, ndarray]:
     """ Searches a Faiss index for the k nearest neighbors of the given query vectors. """
     distances, indices = index.search(query, top_n)
     return distances, indices
 
 
-def faiss_index_remover(index: faiss, ids: list[int]) -> None:
+def faiss_index_remover(index, ids: list[int]) -> None:
     """ Removes vectors from a Faiss index based on their ids. """
     index.remove_ids(array(ids))
 
 
-def faiss_index_storager(index: faiss, file_name: str):
+def faiss_index_storager(index, file_name: str):
     """ Saves a Faiss index to a file. """
-    faiss.write_index(index, f"{file_name}.faiss")
+    write_index(index, f"{file_name}.faiss")
 
 
-def faiss_index_dropper(index: faiss) -> None:
+def faiss_index_dropper(index) -> None:
     """ Drops a Faiss index. """
     index.reset()
 
 
-def faiss_index_loader(file_name: str) -> faiss.Index:
+def faiss_index_loader(file_name: str) -> Index:
     """ Loads a Faiss index from a file. """
-    return faiss.read_index(f"{file_name}.faiss")
+    return read_index(f"{file_name}.faiss")
+
+
+class SeedNP(object):
+    """ A class to seed the numpy random number controller. """
+
+    def __init__(self, seed: int):
+        self._seed = seed
+
+    def __enter__(self):
+        self._state = random.get_state()
+        random.seed(self._seed)
+        print(f"Seeding numpy with {self._seed}.")
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        random.set_state(self._state)
+
+
+class Timer(object):
+    """ A class to measure the time of a code block. """
+
+    def __init__(self, precision: int = 5, description: str = None):
+        self._precision = precision
+        self._desc = description
+        self._elapsed_time = None
+
+    def __enter__(self):
+        self._start = perf_counter()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._end = perf_counter()
+        self._elapsed_time = self._end - self._start
+
+    def __repr__(self):
+        if self._elapsed_time is None:
+            return "Timer not started yet."
+        else:
+            return f"{self._desc} elapsed: {self._elapsed_time:.{self._precision}f} seconds"
